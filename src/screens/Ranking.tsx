fdc0e2a2a1922a81fc1777335c55e0541e448c41
Jawad Usman <jwdusmn@gmail.com>
2025-08-10 14:26:14 -0400

    added dropdown to show character specific rankings

--- src/screens/Ranking.tsx
+++ src/screens/Ranking.tsx
@@ -3,11 +3,13 @@ import "./Ranking.scss";
 import type { RankingObject } from '../types/RankingTypes';
 import RankingBox from '../components/RankingBox';
 import RankingBoxFirst from '../components/RankingBoxFirst';
+import { character_keys } from '../CharacterKeys';
 
 const Ranking:React.FC = () => {
     let rankCounter = 0;
 
     const [rankings, setRankings] = useState<RankingObject[]>([]);
+    const [filteredRankings, setFilteredRankings] = useState<RankingObject[]>([]);
 
     const getRankings = async() => {
         try {
@@ -20,8 +22,20 @@ const Ranking:React.FC = () => {
         }
     };
 
+    const handleCharacterSelect = (char:string) => {
+        if(char == ""){
+            setFilteredRankings(rankings);
+        }else{
+            setFilteredRankings(rankings.filter((e) => {
+                return e.character_id == Number(char)
+            }))
+
+        }
+    }
+
     useEffect(() => {
         console.log(rankings);
+        setFilteredRankings(rankings);
     }, [rankings]);
 
     useEffect(() => {
@@ -30,9 +44,35 @@ const Ranking:React.FC = () => {
 
     return(
         <div>
-            <table className={"Ranking"}>
-                <tbody>
-                    {rankings.map((e) =>{
+            <div className='instructions'>
+                <div>
+                    How To Join
+                </div>
+                <div>
+                    To join the rankings you first need to have the BBCF Improvement Mod which you can find <a href='https://github.com/libreofficecalc/BBCF-Improvement-Mod'>here</a>. Once you've 
+                    followed the instructions to install the mod, go into the settings.ini file and add this 
+                    to a new line: UploadReplayDataHost: 18.219.55.213 then save and close the file. After doing this make sure the replay 
+                    upload in the mod is enabled and in your bbcf settings in the Network Options, Saving Replays 
+                    for Ranked, Player, and Lobby Match should be set to Allow. After that whenever you fight someone 
+                    else who is also participating in this ranking, results and ratings should be automatically uploaded 
+                    and calculated.
+                </div>
+            </div>
+            <select
+                onChange={e => handleCharacterSelect(e.target.value)}
+            >
+                <option key={"empty"} value={""}>{"All"}</option>
+                {
+                    character_keys.map((e, index) => {
+                        return(
+                            <option key={index.toString()} value={index}>{e.name}</option>
+                        )
+                    })
+                }
+            </select>
+            <div className={"Ranking"}>
+                {filteredRankings.length > 0?<div>
+                    {filteredRankings.map((e) =>{
                         if(e.matches.length != 0){
                             rankCounter += 1;
                             return(
@@ -48,8 +88,10 @@ const Ranking:React.FC = () => {
                             )
                         }
                     })} 
-                </tbody>
-            </table>
+                </div>:
+                <div>No rankings for this character</div>
+                }
+            </div>
         </div>
     );
 };
