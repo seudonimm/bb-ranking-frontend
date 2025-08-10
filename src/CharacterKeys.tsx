631acbb4963fe670c40056d44d53973f97752512
Jawad Usman <jwdusmn@gmail.com>
2025-08-10 11:21:14 -0400

    added full_body_url for all characters

--- src/CharacterKeys.tsx
+++ src/CharacterKeys.tsx
@@ -1,23 +1,29 @@
 export const character_keys = [
     {
         name:"Ragna",
-        icon_url: "https://www.dustloop.com/wiki/images/1/1a/BBCF_Ragna_the_Bloodedge_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/1/1a/BBCF_Ragna_the_Bloodedge_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/2/2f/BBCF_Ragna_Portrait.png"
+
     },
     {
         name:"Jin",
-        icon_url: "https://www.dustloop.com/wiki/images/1/1e/BBCF_Jin_Kisaragi_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/1/1e/BBCF_Jin_Kisaragi_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/5/50/BBCF_Jin_Portrait.png"
     },
     {
         name:"Noel",
-        icon_url: "https://www.dustloop.com/wiki/images/e/e8/BBCF_Noel_Vermillion_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/e/e8/BBCF_Noel_Vermillion_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/2/2e/BBCF_Noel_Portrait.png"
     },
     {
         name:"Rachel",
-        icon_url: "https://www.dustloop.com/wiki/images/6/63/BBCF_Rachel_Alucard_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/6/63/BBCF_Rachel_Alucard_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/7/7e/BBCF_Rachel_Portrait.png"
     },
     {
         name:"Taokaka",
-        icon_url: "https://www.dustloop.com/wiki/images/8/80/BBCF_Taokaka_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/8/80/BBCF_Taokaka_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/0/02/BBCF_Taokaka_Portrait.png"
     },
     {
         name:"Tager",
@@ -26,123 +32,153 @@ export const character_keys = [
     },
     {
         name:"Litchi",
-        icon_url: "https://www.dustloop.com/wiki/images/e/ed/BBCF_Litchi_Faye_Ling_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/e/ed/BBCF_Litchi_Faye_Ling_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/0/07/BBCF_Litchi_Portrait.png"
     },
     {
         name:"Arakune",
-        icon_url: "https://www.dustloop.com/wiki/images/d/df/BBCF_Arakune_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/d/df/BBCF_Arakune_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/3/3b/BBCF_Arakune_Portrait.png"
     },
     {
         name:"Bang",
-        icon_url: "https://www.dustloop.com/wiki/images/a/a5/BBCF_Bang_Shishigami_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/a/a5/BBCF_Bang_Shishigami_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/d/d4/BBCF_Bang_Portrait.png"
     },
     {
         name:"Carl",
-        icon_url: "https://www.dustloop.com/wiki/images/1/1f/BBCF_Carl_Clover_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/1/1f/BBCF_Carl_Clover_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/a/a7/BBCF_Carl_Portrait.png"
     },
     {
         name:"Hakumen",
-        icon_url: "https://www.dustloop.com/wiki/images/4/4a/BBCF_Hakumen_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/4/4a/BBCF_Hakumen_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/5/52/BBCF_Hakumen_Portrait.png"
     },
     {
         name:"Nu",
-        icon_url: "https://www.dustloop.com/wiki/images/1/10/BBCF_Nu-13_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/1/10/BBCF_Nu-13_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/3/31/BBCF_Nu_Portrait.png"
     },
     {
         name:"Tsubaki",
-        icon_url: "https://www.dustloop.com/wiki/images/6/67/BBCF_Tsubaki_Yayoi_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/6/67/BBCF_Tsubaki_Yayoi_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/b/b3/BBCF_Tsubaki_Portrait.png"
     },
     {
         name:"Hazama",
-        icon_url: "https://www.dustloop.com/wiki/images/d/db/BBCF_Hazama_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/d/db/BBCF_Hazama_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/f/f7/BBCF_Hazama_Portrait.png"
     },
     {
         name:"Mu",
-        icon_url: "https://www.dustloop.com/wiki/images/b/b1/BBCF_Mu-12_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/b/b1/BBCF_Mu-12_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/e/e4/BBCF_Mu_Portrait.png"
     },
     {
         name:"Makoto",
-        icon_url: "https://www.dustloop.com/wiki/images/6/6a/BBCF_Makoto_Nanaya_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/6/6a/BBCF_Makoto_Nanaya_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/c/cc/BBCF_Makoto_Portrait.png"
     },
     {
         name:"Valkenhayn",
-        icon_url: "https://www.dustloop.com/wiki/images/6/68/BBCF_Valkenhayn_R._Hellsing_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/6/68/BBCF_Valkenhayn_R._Hellsing_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/e/e3/BBCF_Valkenhayn_Portrait.png"
     },
     {
         name:"Platinum",
-        icon_url: "https://www.dustloop.com/wiki/images/d/da/BBCF_Platinum_the_Trinity_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/d/da/BBCF_Platinum_the_Trinity_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/f/fe/BBCF_Platinum_Portrait.png"
     },
     {
         name:"Relius",
-        icon_url: "https://www.dustloop.com/wiki/images/7/73/BBCF_Relius_Clover_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/7/73/BBCF_Relius_Clover_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/6/65/BBCF_Relius_Portrait.png"
     },
     {
         name:"Izayoi",
-        icon_url: "https://www.dustloop.com/wiki/images/f/f2/BBCF_Izayoi_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/f/f2/BBCF_Izayoi_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/9/96/BBCF_Izayoi_Portrait.png"
     },
     {
         name:"Amane",
-        icon_url: "https://www.dustloop.com/wiki/images/8/86/BBCF_Amane_Nishiki_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/8/86/BBCF_Amane_Nishiki_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/2/23/BBCF_Amane_Portrait.png"
     },
     {
         name:"Bullet",
-        icon_url: "https://www.dustloop.com/wiki/images/1/17/BBCF_Bullet_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/1/17/BBCF_Bullet_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/8/89/BBCF_Bullet_Portrait.png"
     },
     {
         name:"Azrael",
-        icon_url: "https://www.dustloop.com/wiki/images/7/71/BBCF_Azrael_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/7/71/BBCF_Azrael_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/7/7d/BBCF_Azrael_Portrait.png"
     },
     {
         name:"Kagura",
-        icon_url: "https://www.dustloop.com/wiki/images/7/72/BBCF_Kagura_Mutsuki_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/7/72/BBCF_Kagura_Mutsuki_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/5/5f/BBCF_Kagura_Portrait.png"
     },
     {
         name:"Kokonoe",
-        icon_url: "https://www.dustloop.com/wiki/images/2/2b/BBCF_Kokonoe_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/2/2b/BBCF_Kokonoe_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/1/10/BBCF_Kokonoe_Portrait.png"
     },
     {
         name:"Terumi",
-        icon_url: "https://www.dustloop.com/wiki/images/e/ee/BBCF_Yuuki_Terumi_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/e/ee/BBCF_Yuuki_Terumi_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/7/70/BBCF_Terumi_Portrait.png"
     },
     {
         name:"Celica",
-        icon_url: "https://www.dustloop.com/wiki/images/a/a1/BBCF_Celica_A._Mercury_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/a/a1/BBCF_Celica_A._Mercury_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/1/1e/BBCF_Celica_Portrait.png"
     },
     {
         name:"Lambda",
-        icon_url: "https://www.dustloop.com/wiki/images/2/28/BBCF_Lambda-11_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/2/28/BBCF_Lambda-11_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/1/13/BBCF_Lambda_Portrait.png"
     },
     {
         name:"Hibiki",
-        icon_url: "https://www.dustloop.com/wiki/images/f/fb/BBCF_Hibiki_Kohaku_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/f/fb/BBCF_Hibiki_Kohaku_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/1/13/BBCF_Hibiki_Portrait.png"
     },
     {
         name:"Nine",
-        icon_url: "https://www.dustloop.com/wiki/images/1/1f/BBCF_Nine_the_Phantom_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/1/1f/BBCF_Nine_the_Phantom_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/7/78/BBCF_Nine_Portrait.png"
     },
     {
         name:"Naoto",
-        icon_url: "https://www.dustloop.com/wiki/images/2/2e/BBCF_Naoto_Kurogane_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/2/2e/BBCF_Naoto_Kurogane_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/0/04/BBCF_Naoto_Portrait.png"
     },
     {
         name:"Izanami",
-        icon_url: "https://www.dustloop.com/wiki/images/1/13/BBCF_Izanami_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/1/13/BBCF_Izanami_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/d/d5/BBCF_Izanami_Portrait.png"
     },
     {
         name:"Susanoo",
-        icon_url: "https://www.dustloop.com/wiki/images/6/64/BBCF_Susano%27o_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/6/64/BBCF_Susano%27o_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/1/1f/BBCF_Susanoo_Portrait.png"
     },
     {
         name:"Es",
-        icon_url: "https://www.dustloop.com/wiki/images/a/a1/BBCF_Es_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/a/a1/BBCF_Es_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/c/c1/BBCF_Es_Portrait.png"
     },
     {
         name:"Mai",
-        icon_url: "https://www.dustloop.com/wiki/images/a/a2/BBCF_Mai_Natsume_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/a/a2/BBCF_Mai_Natsume_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/3/36/BBCF_Mai_Portrait.png"
     },
     {
         name:"Jubei",
-        icon_url: "https://www.dustloop.com/wiki/images/6/63/BBCF_Jubei_Icon.png"
+        icon_url: "https://www.dustloop.com/wiki/images/6/63/BBCF_Jubei_Icon.png",
+        full_body_url:"https://www.dustloop.com/wiki/images/c/c1/BBCF_Jubei_Portrait.png"
     },
-    ]
+]
 
